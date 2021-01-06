08dceccd14fb7ba47fc2768d44f674cd5950551e
Caitlyn <cgreffly@gmail.com>
2021-01-05 18:17:13 -0800

    delete helper-compilation-targets

--- public/render-page.js
+++ public/render-page.js
@@ -118,13 +118,13 @@ var plugins = [{
     "background_color": "#663399",
     "theme_color": "#663399",
     "display": "minimal-ui",
-    "icon": "content/assets/FfT_Logo_Thumbnail.png",
+    "icon": "src/images/favicon.png",
     "legacy": true,
     "theme_color_in_head": true,
     "cache_busting_mode": "query",
     "crossOrigin": "anonymous",
     "include_favicon": true,
-    "cacheDigest": null
+    "cacheDigest": "7613fd1838a9c74a2d8ce9947bae8b1b"
   }
 }]; // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
